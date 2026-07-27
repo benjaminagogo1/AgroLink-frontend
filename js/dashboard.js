b64844e60e2d9c82451c9d1e40a98f4d9c4b4c0a
benjaminagogo1 <josephbenjamin046@gmail.com>
2026-07-27 16:43:38 +0100

    Agro-Shield-frontend

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,61 @@
-const userName = "Benjamin";
+const counters = document.querySelectorAll(".counter");
 
-const welcomeMessage = document.querySelector("main h2");
+counters.forEach(counter => {
 
-welcomeMessage.textContent = `Welcome, ${userName}!`;
+    const target = Number(counter.dataset.target);
+
+    let count = 0;
+
+    const speed = Math.ceil(target / 100);
+
+    function updateCounter() {
+
+        if (count < target) {
+
+            count += speed;
+
+            if (count > target) {
+                count = target;
+            }
+
+            counter.textContent = count.toLocaleString();
+
+            requestAnimationFrame(updateCounter);
+
+        }
+
+    }
+
+    updateCounter();
+
+});
+
+// Greeting
+
+const greeting = document.getElementById("greeting");
+
+const hour = new Date().getHours();
+
+if (hour < 12) {
+
+    greeting.textContent = "Good Morning";
+
+}
+
+else if (hour < 17) {
+
+    greeting.textContent = "Good Afternoon";
+
+}
+
+else {
+
+    greeting.textContent = "Good Evening";
+
+}
+
+// Current Date
+
+const today = document.getElementById("today");
+
+today.textContent = new Date().toDateString();
